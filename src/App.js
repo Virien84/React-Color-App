e38838850d831a91570aebf43bdfc2648502e952
David Antón Álvarez <david.anton.dev@gmail.com>
2022-01-05 19:57:33 +0100

    Finish MiniPalette delete

--- src/App.js
+++ src/App.js
@@ -22,6 +22,14 @@ class App extends Component {
       return palette.id === id;
     });
   }
+  deletePalette(id) {
+    this.setState(
+      (st) => ({
+        palettes: st.palettes.filter((palette) => palette.id !== id),
+      }),
+      this.syncLocalStorage
+    );
+  }
   savePalette(newPalette) {
     this.setState(
       { palettes: [...this.state.palettes, newPalette] },
@@ -64,7 +72,11 @@ class App extends Component {
           exact
           path="/"
           render={(routeProps) => (
-            <PaletteList palettes={this.state.palettes} {...routeProps} />
+            <PaletteList
+              palettes={this.state.palettes}
+              deletePalette={this.deletePalette}
+              {...routeProps}
+            />
           )}
         />
         <Route
